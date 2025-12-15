b02bc588eb693c82bfdcc8ab90272c1fad35387e
tbiswas1074 <biswastuhin1074@gmail.com>
2025-12-15 17:17:41 +0530

    Rest template first commit

---
+++ ServiceTow/src/test/java/com/example/ServiceTow/ServiceTowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiceTow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
